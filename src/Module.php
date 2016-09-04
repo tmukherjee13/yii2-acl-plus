2c9aa4239b3e7c61cf262b9604e8a1f5399971fd
Tarun M <tmukherjee13@gmail.com>
2016-09-04 22:57:20 +0530

    bootstrapped urls

--- src/Module.php
+++ src/Module.php
@@ -7,26 +7,18 @@ class Module extends \yii\base\Module
     
     public function init() {
         parent::init();
-        die('asdfds');
+        //die('asdfds');
         // custom initialization code goes here
         
         // initialize the module with the configuration loaded from config.php
         // \Yii::configure($this, require (__DIR__ . '/config/config.php'));
-        $sFilePathConfig = __DIR__ . '/config/_routes.php';
-        echo $sFilePathConfig;
-        die;
-        if (file_exists($sFilePathConfig)) {
-            \Yii::$app->getUrlManager()->addRules(require ($sFilePathConfig));
-        }
+        // $sFilePathConfig = __DIR__ . '/config/_routes.php';
+        // // echo $sFilePathConfig;
+        // // die;
+        // if (file_exists($sFilePathConfig)) {
+        //     \Yii::$app->getUrlManager()->addRules(require ($sFilePathConfig));
+        // }
         
     }
     
-    // public function bootstrap($app) {
-        
-    //     parent::bootstrap();
-    //     $sFilePathConfig = __DIR__ . '/config/_routes.php';
-    //     if (file_exists($sFilePathConfig)) {
-    //         $app->getUrlManager()->addRules(require ($sFilePathConfig));
-    //     }
-    // }
 }
